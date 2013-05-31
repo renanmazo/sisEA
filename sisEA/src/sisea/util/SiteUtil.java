fb4f17e41175224295691d18587ae4dbe93e0f85
dbelmontee <deborabelmontee@hotmail.com>
2013-05-31 14:44:43 -0300

    Coisas uteis pra qualquer classe vai aqui

---
+++ sisEA/src/sisea/util/SiteUtil.java
@@ -0,0 +1,33 @@
+package sisea.util;
+
+public final class SiteUtil {
+	
+	public static boolean isEmptyOrNull(Object object) {
+		return object == null || object.toString().trim().equals("");
+	}
+	
+	public static boolean isEmptyOrNullOrZero(Object valor) {
+		return isEmptyOrNull(valor)
+				|| valor.toString().equals(String.valueOf(0));
+	}
+	
+	public static String prioridadeLiteral(int prioridade){
+		switch (prioridade) {
+		case 1:
+			return "Altíssima";
+		case 2:
+			return "Alta";
+		case 3:
+			return "Média";
+		case 4:
+			return "Baixa";
+		case 5:
+			return "Baixíssima";
+		default:
+			return "";
+		}
+		
+	}
+
+
+}
