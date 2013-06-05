34d465f5799941b8c436ae3ec009432e529d2d0a
dbelmontee <deborabelmontee@hotmail.com>
2013-06-05 19:00:19 -0300

    Atividade Alt, Inc

---
+++ sisEA/src/sisea/view/atividade/AtividadeAltBean.java
@@ -0,0 +1,11 @@
+package sisea.view.atividade;
+
+import sisea.model.Atividade;
+
+public class AtividadeAltBean {
+	
+	public String iniciarPagina(Atividade atividade){
+		return "IR_PARA_ATIVIDADE_ALT";
+	}
+
+}
---
+++ sisEA/src/sisea/view/atividade/AtividadeIncBean.java
@@ -0,0 +1,10 @@
+package sisea.view.atividade;
+
+public class AtividadeIncBean {
+	
+	public String iniciarPagina(){
+		
+		return "IR_PARA_ATIVIDADE_INC";
+	}
+
+}
