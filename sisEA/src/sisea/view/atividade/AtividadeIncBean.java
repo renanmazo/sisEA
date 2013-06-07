c1d12e09f3bf21318244091874c27edbde2af7fb
dbelmontee <deborabelmontee@hotmail.com>
2013-06-07 19:51:31 -0300

    Pegando o bean pela sessão

--- sisEA/src/sisea/view/atividade/AtividadeIncBean.java
+++ sisEA/src/sisea/view/atividade/AtividadeIncBean.java
@@ -5,39 +5,55 @@ import java.util.List;
 
 import javax.faces.model.SelectItem;
 
+import org.primefaces.model.DualListModel;
+
 import sisea.model.Atividade;
 import sisea.service.combos.CombosService;
 
 public class AtividadeIncBean {
 	private CombosService combosService = new CombosService();
-	
+
 	private Atividade atividade;
 	private List<SelectItem> comboPrioridade;
 	private List<SelectItem> comboProjeto;
-	
-	public String iniciarPagina(){
+	private List<SelectItem> comboStatus;
+	private DualListModel<String> cities;
+
+	public DualListModel<String> getCities() {
+		return cities;
+	}
+
+	public void setCities(DualListModel<String> cities) {
+		this.cities = cities;
+	}
+
+	public AtividadeIncBean() {
+		List<String> citiesSource = new ArrayList<String>();
+		List<String> citiesTarget = new ArrayList<String>();
+		citiesSource.add("Istanbul");
+		citiesSource.add("Ankara");
+		citiesSource.add("Izmir");
+		citiesSource.add("Antalya");
+		citiesSource.add("Bursa");
+
+		cities = new DualListModel<String>(citiesSource, citiesTarget);
+	}
+
+	public String iniciarPagina() {
 		inicializar();
 		carregaComboPrioridade();
 		carregaComboProjeto();
+		carregaComboStatus();
 		return "atividadeInc.xhtml?faces-redirect=true";
 	}
-	
-	public void inicializar(){
+
+	public void inicializar() {
 		setAtividade(new Atividade());
 		setComboPrioridade(new ArrayList<SelectItem>());
 		setComboProjeto(new ArrayList<SelectItem>());
 	}
-	
-	public void carregaComboPrioridade(){
-		setComboPrioridade(getCombosService().carregarComboPrioridades());
-		getComboPrioridade();
-	}
-	
-	public void carregaComboProjeto(){
-		setComboProjeto(getCombosService().carregaComboProjeto());
-		getComboProjeto();
-	}
 
+	// COMBO DE ATIVIDADES
 	public Atividade getAtividade() {
 		return atividade;
 	}
@@ -46,6 +62,7 @@ public class AtividadeIncBean {
 		this.atividade = atividade;
 	}
 
+	// COMBO dE PRIORIDADES
 	public List<SelectItem> getComboPrioridade() {
 		return comboPrioridade;
 	}
@@ -54,14 +71,37 @@ public class AtividadeIncBean {
 		this.comboPrioridade = comboPrioridade;
 	}
 
+	public void carregaComboPrioridade() {
+		setComboPrioridade(getCombosService().carregarComboPrioridades());
+	}
+
+	// COMBO DE PROJETOS
 	public List<SelectItem> getComboProjeto() {
-		return comboProjeto;
+		return this.comboProjeto;
 	}
 
 	public void setComboProjeto(List<SelectItem> comboProjeto) {
 		this.comboProjeto = comboProjeto;
 	}
 
+	public void carregaComboProjeto() {
+		setComboProjeto(getCombosService().carregaComboProjeto());
+	}
+
+	// COMBO DE STATUS
+	public void setComboStatus(List<SelectItem> comboStatus) {
+		this.comboStatus = comboStatus;
+	}
+
+	public List<SelectItem> getComboStatus() {
+		return this.comboStatus;
+	}
+
+	public void carregaComboStatus() {
+		setComboStatus(getCombosService().carregaComboStatus());
+	}
+
+	// SERVICE
 	public CombosService getCombosService() {
 		return combosService;
 	}
