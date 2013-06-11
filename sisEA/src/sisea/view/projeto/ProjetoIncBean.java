6f7c48ce2568248bf279ef85874da81c089a0de0
Renan <renanmazo@hotmail.com>
2013-06-11 01:47:37 -0300

    Quando possível, gostaria de ver você fazer esse

--- sisEA/src/sisea/view/projeto/ProjetoIncBean.java
+++ sisEA/src/sisea/view/projeto/ProjetoIncBean.java
@@ -1,85 +1,14 @@
 package sisea.view.projeto;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.faces.model.SelectItem;
-
-import org.primefaces.model.DualListModel;
-
-import sisea.model.Atividade;
-import sisea.model.Habilidade;
-import sisea.service.combos.CombosService;
-
 public class ProjetoIncBean {
-	private CombosService combosService = new CombosService();
-
-	private Atividade atividade;
-	private List<SelectItem> comboPrioridade;
-	private List<SelectItem> comboProjeto;
-	private List<SelectItem> comboStatus;
-	private DualListModel<Habilidade> habilidades;
 
 	public String iniciarPagina() {
 		inicializar();
-		carregaComboStatus();
-		return "atividadeInc.xhtml?faces-redirect=true";
+		return "IR_PARA_PROJETO_INC";
 	}
 
 	public void inicializar() {
-		setAtividade(new Atividade());
-		setComboStatus(new ArrayList<SelectItem>());
-	}
-
-	public void carregaComboStatus() {
-		setComboStatus(getCombosService().carregaComboStatus());
-	}
-
-	public CombosService getCombosService() {
-		return combosService;
-	}
-
-	public void setCombosService(CombosService combosService) {
-		this.combosService = combosService;
-	}	
-	public Atividade getAtividade() {
-		return atividade;
-	}
-
-	public void setAtividade(Atividade atividade) {
-		this.atividade = atividade;
-	}
-
-	public List<SelectItem> getComboPrioridade() {
-		return comboPrioridade;
-	}
-
-	public void setComboPrioridade(List<SelectItem> comboPrioridade) {
-		this.comboPrioridade = comboPrioridade;
-	}
-
-	public List<SelectItem> getComboProjeto() {
-		return this.comboProjeto;
-	}
-
-	public void setComboProjeto(List<SelectItem> comboProjeto) {
-		this.comboProjeto = comboProjeto;
-	}
-	
-	public void setComboStatus(List<SelectItem> comboStatus) {
-		this.comboStatus = comboStatus;
-	}
-
-	public List<SelectItem> getComboStatus() {
-		return this.comboStatus;
-	}	
-
-	public DualListModel<Habilidade> getHabilidades() {
-		return habilidades;
-	}
-
-	public void setHabilidades(DualListModel<Habilidade> habilidades) {
-		this.habilidades = habilidades;
+		
 	}
 
 }
